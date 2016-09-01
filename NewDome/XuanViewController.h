e287af5c2ca0e7623fa9d3214ff2470647e36439
Hanyanrui <13356390867@163.com>
2016-09-01 21:28:27 +0800

    韩燕瑞
    新项目

---
+++ NewDome/XuanViewController.h
@@ -0,0 +1,13 @@
+//
+//  XuanViewController.h
+//  NewDome
+//
+//  Created by student on 16/9/1.
+//  Copyright © 2016年 HYR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XuanViewController : UIViewController
+
+@end
---
+++ NewDome/XuanViewController.m
@@ -0,0 +1,37 @@
+//
+//  XuanViewController.m
+//  NewDome
+//
+//  Created by student on 16/9/1.
+//  Copyright © 2016年 HYR. All rights reserved.
+//
+
+#import "XuanViewController.h"
+
+@interface XuanViewController ()
+
+@end
+
+@implementation XuanViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
